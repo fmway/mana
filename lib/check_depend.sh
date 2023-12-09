0561490c097bb51fd296b31d6ff21337c9ac01b0
luisadha <luisadha@noreply.codeberg.org>
2023-12-09 12:47:18 +0000

    Add lib/check_depend.sh

---
+++ lib/check_depend.sh
@@ -0,0 +1,6 @@
+function check_dependency {
+if ! (builtin command -V "$1" >/dev/null 2>&1); then
+echo "missing dependency: can't find $1"  
+return 1            
+  fi
+}                                          
