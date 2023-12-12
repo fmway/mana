2a623ae9aaa548bbaf489d41d6c6ae57b0eebaf0
hitagi <alifnc@proton.me>
2023-12-12 09:01:34 +0700

    added gitignore

--- lib/install.sh
+++ lib/install.sh
@@ -2,12 +2,35 @@ E_NOARGS="75"
 
 # function to handle source code based package building.
 _sourceBuild(){
+  # find packages in the repo.
+  searchP=$(find ../templates -name "$1.mana") 
 
+  if [ -z $searchP ]; then
+    echo "Package not found."
+    exit $E_NOARGS
+  fi
+
+  # put .mana intruction build.
+  source "$searchP"
+
+  # set the source code directory.
+  SRC="${MANA_WORKDIR}/${name}-${version}"
+
+  # get the source code from the source URL.
+  echo $(wget -P $MANA_DISTDIR $sourceURL)
+
+  # unpack the source code.
+  tar xvf $MANA_DISTDIR/$name-$version.tar.gz -C $MANA_WORKDIR 
+
+  # build and install to the stagged environment.
+  build
+
+  return $?
 }
 
 # function to handle binary package building.
 _binBuild(){
-
+  echo "Binary build."
 }
 
 _install(){
@@ -16,17 +39,13 @@ _install(){
     exit $E_NOARGS
   fi
 
-  # find packages in the repo.
-  searchP=$(find ../templates -name "$1.mana")
+  # identifies the package to be installed, whether binary or source code based.
+  # if [ $searchP = *-bin.mana ]; then
+  #   _binBuild
+  #   return $?
+  # fi
 
-  # if the package is found, download the source code and build the package.
-  if [ -z $searchP ]; then
-    echo "installing $1"
-    source "../templates/bash.mana"
-    
-    SRC="${MANA_WORKDIR}/${name}-${version}"
-    #echo $(wget -P $MANA_DISTDIR $sourceURL)
-    build
+  _sourceBuild $1
 
-  fi
+  return $?
 }
