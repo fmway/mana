9f0e1ad811b3ae7b15bcd72f2886bf60a854b1bd
hitagi <alifnc@proton.me>
2023-12-18 12:11:34 +0700

    added remove functionality

---
+++ lib/remove.sh
@@ -0,0 +1,24 @@
+E_NOARGS=75
+
+_remove(){
+  # package to delete: $1.
+  if [ -z $1 ]; then
+    echo "Enter the name of the package to remove"
+    exit $E_NOARGS
+  fi
+
+  # delete installed package files based on footprint.
+  cd $MANA_ROOTDIR
+
+  for i in $(cut -d' ' -f2 $MANA_DBDIR/$1/footprint); do
+    rm -v $i
+  done
+
+  # delete empty directory.
+  find $MANA_ROOTDIR/etc $MANA_ROOTDIR/opt $MANA_ROOTDIR/usr -depth -type d -empty -exec rmdir -v {} \;
+
+  # remove from the wordlist (/var/db/mana/world).
+  sed -i "/$1/d" $MANA_DBDIR/world
+
+  exit $?
+}
