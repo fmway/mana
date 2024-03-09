d380306094ee50068baa49133c4101897e2464f3
hitagi <alifnc@proton.me>
2024-03-09 13:22:11 +0700

    improve reusability

--- lib/footprint.sh
+++ lib/footprint.sh
@@ -1,15 +1,15 @@
 _footprint(){
   # enter the stage environment.
-  cd $MANA_STAGEDIR
+  cd $1
 
   # create directory if it does not exist.
-  if [ -z $(find $MANA_DBDIR -type d -name "$name") ]; then
-    mkdir -p $MANA_DBDIR/$name || (echo "Cannot create database. Aborting." && exit 1)
+  if [ -z $(find $2 -type d -name "$name") ]; then
+    mkdir -p $2 || (echo "Cannot create database. Aborting." && exit 1)
   fi
 
   # save footprints into a database.
   for i in $(find * \! -type d); do
     permission=$(stat -c "%a" $i)
-    echo "$permission $i" >> $MANA_DBDIR/$name/FOOTPRINT || (echo "Cannot write database. Aborting." && exit 1)
+    echo "$permission $i" >> $2/FOOTPRINT || (echo "Cannot write database. Aborting." && exit 1)
   done
 }
